865ff53162374d0eb2976f6854be7ea76e57afd7
Besufkad Tessema <104687723+BesufkadGirma@users.noreply.github.com>
2022-06-16 00:18:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(let i=0; i<numberOfDrumButtons; i++){
+document.querySelectorAll("button")[i].addEventListener('click',  function (){
+let key=this.innerHTML;
+makeSound(key);
+buttonAnimation(key);
+});
+}
+document.addEventListener('keypress', function(event){
+    let pressed_key = event.key;
+    makeSound(pressed_key);
+    buttonAnimation(pressed_key);
+});
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            let tom1=new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            let tom2=new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            let tom3=new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            let tom4=new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            let snare=new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            let crash=new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            let kick=new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            console.log(key);
+    
+    }
+}
+function buttonAnimation(key){
+let activeKey = document.querySelector("."+key);
+activeKey.classList.add("pressed");
+setTimeout(function(){
+    activeKey.classList.remove("pressed");
+}, 100);
+}
